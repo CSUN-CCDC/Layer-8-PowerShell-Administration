b89f8b5c8ef8ffc199987672def3d1a1f2d8e0eb
yellowpluto <yellowpluto31569@gmail.com>
2025-09-01 14:09:06 -0700

    psswrdshfl function

--- Layer 8 PowerShell Administration.ps1
+++ Layer 8 PowerShell Administration.ps1
@@ -420,11 +420,14 @@ while ($start -eq $true) {
 
 	#Is the verb 'Shuffle' ok?
 	function Set-RandomADPasswords {
+		#add random characters
 		if (!(Test-Path -Path "C:\output")) {
 			New-Item -ItemType Directory -Path "C:\output"
 		} 
+		$t = get-date -format yyyymmdd_HHmm
+		$fileName = "pwFile.txt"
+
 		$output = @()
-		$fileName = Read-Host "Enter filename. File will be outputted to C:\output as .txt"
 		$Users = Get-ADUser -Filter * | Select-Object -ExpandProperty Name
 		foreach ($User in $Users) {
 			$passphrase = $null
@@ -438,24 +441,23 @@ while ($start -eq $true) {
 				$txtFile = Get-Random -Max 4 -Min 1
 				switch ($txtFile) {
 					1 {
-						# We don't want repeats of words
-						if ($null -ne $noun) {
+			   # We don't want repeats of words
+			   if ($null -ne $noun) {
 							break
-						}
-						$noun = Get-Random -InputObject (Get-Content .\Nouns.txt)
-						$count++
-						$passphrase += $noun + "-"
-	   
+			   }
+			   $noun = Get-Random -InputObject (Get-Content .\Nouns.txt)
+			   $count++
+			   $passphrase += $noun + "-"
 					}
 
 					2 {
-						if ($null -ne $verb) {
+			   if ($null -ne $verb) {
 							break
-						} 
-						$verb = Get-Random -InputObject (Get-Content .\Verbs.txt)
-						$count++
-						$passphrase += $verb + "-"
-	   
+			   } 
+			   $verb = Get-Random -InputObject (Get-Content .\Verbs.txt)
+			   $count++
+			   $passphrase += $verb + "-"
+			   
 					}
 
 					3 {
@@ -465,22 +467,29 @@ while ($start -eq $true) {
 						$adjective = Get-Random -InputObject (Get-Content .\Adjectives.txt)
 						$count++
 						$passphrase += $adjective + "-"
-		
 					}
 
 				}
 			}
+	
 			#Builds the final passphrase and sets it
 			$passphrase += $randomEndNumber
 			$securePassword = ConvertTo-SecureString -String $passphrase -AsPlainText -Force
-			Set-ADAccountPassword -Identity $User -NewPassword $securePassword -Credential $tempCred
+			Set-ADAccountPassword -Identity $User -NewPassword $securePassword -Credential $credential
 			$output = @("$User" + ": " + "$passphrase")
+			# Writing current user's PW to the PW file and loop to get the next user.
 			$output | Out-File -FilePath "C:\output\$fileName.txt" -Append
+			Invoke-Item -Path "C:\output\"
 		}
+
+		# Writing out the dynamic file
+		$fileName = "pwFile_$t.txt"
+		$output | Out-File -FilePath "C:\output\$fileName.txt"
+
 	}
 
 	#PingInfoView may not be allowed during comp...will get to later
-	function Use-PingInfoView{
+	function Use-PingInfoView {
 
 	}
 
