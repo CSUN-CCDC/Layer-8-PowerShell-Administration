dbd6aeaff2e6cbe3abf12ac764b3ebf2b1ded13b
yellowpluto <yellowpluto31569@gmail.com>
2025-09-01 16:02:06 -0700

    psswrdshfl fix

--- Layer 8 PowerShell Administration.ps1
+++ Layer 8 PowerShell Administration.ps1
@@ -427,11 +427,15 @@ while ($start -eq $true) {
 		if (!(Test-Path -Path "C:\output")) {
 			New-Item -ItemType Directory -Path "C:\output"
 		} 
+		
 		$t = get-date -format yyyymmdd_HHmm
-		$fileName = "pwFile.txt"
-
-		$output = @()
-		$Users = Get-ADUser -Filter * | Select-Object -ExpandProperty Name
+		$fileName = "pwFile"
+		
+		if ((Test-Path -Path "C:\output\pwFile.txt") -eq ($true)) {
+			Clear-Content -Path "C:\output\pwFile.txt"
+		}
+		
+		$Users = Get-ADUser -Filter * | Select-Object -ExpandProperty SamAccountName
 		foreach ($User in $Users) {
 			$passphrase = $null
 			$randomEndNumber = Get-Random -Max 99999 -Min 10000
@@ -448,7 +452,7 @@ while ($start -eq $true) {
 			   if ($null -ne $noun) {
 							break
 			   }
-			   $noun = Get-Random -InputObject (Get-Content .\Nouns.txt)
+			   $noun = Get-Random -InputObject (Get-Content "$PSScriptRoot\Passphrases\Nouns.txt")
 			   $count++
 			   $passphrase += $noun + "-"
 					}
@@ -457,7 +461,7 @@ while ($start -eq $true) {
 			   if ($null -ne $verb) {
 							break
 			   } 
-			   $verb = Get-Random -InputObject (Get-Content .\Verbs.txt)
+			   $verb = Get-Random -InputObject (Get-Content "$PSScriptRoot\Passphrases\Verbs.txt")
 			   $count++
 			   $passphrase += $verb + "-"
 			   
@@ -467,7 +471,7 @@ while ($start -eq $true) {
 						if ($null -ne $adjective) {
 							break
 						}
-						$adjective = Get-Random -InputObject (Get-Content .\Adjectives.txt)
+						$adjective = Get-Random -InputObject (Get-Content "$PSScriptRoot\Passphrases\Adjectives.txt")
 						$count++
 						$passphrase += $adjective + "-"
 					}
@@ -480,14 +484,16 @@ while ($start -eq $true) {
 			$securePassword = ConvertTo-SecureString -String $passphrase -AsPlainText -Force
 			Set-ADAccountPassword -Identity $User -NewPassword $securePassword -Credential $credential
 			$output = @("$User" + ": " + "$passphrase")
+			$dynamicFile += $output
 			# Writing current user's PW to the PW file and loop to get the next user.
 			$output | Out-File -FilePath "C:\output\$fileName.txt" -Append
 			Invoke-Item -Path "C:\output\"
 		}
 
 		# Writing out the dynamic file
-		$fileName = "pwFile_$t.txt"
-		$output | Out-File -FilePath "C:\output\$fileName.txt"
+		
+		$fileName = "pwFile_$t"
+		$dynamicFile | Out-File -FilePath "C:\output\$fileName.txt"
 
 	}
 
