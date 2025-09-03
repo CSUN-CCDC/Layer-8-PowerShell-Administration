db04cfe0ffadfa87e74a58793c9b1d1bd17a0d16
yellowpluto <yellowpluto31569@gmail.com>
2025-09-03 15:47:49 -0700

    chocolatey?

--- Layer 8 PowerShell Administration.ps1
+++ Layer 8 PowerShell Administration.ps1
@@ -172,7 +172,7 @@ function Enable-PSRemotingInDomain {
 #UNFINISHED SCRIPT
 function Install-ChocolateyInDomain {
 	Invoke-Command {
-    
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 	}
 }
 
